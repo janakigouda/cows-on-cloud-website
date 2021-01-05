816e8eacd588b01d7246a346e85b90e23c31a666
janakigouda <janakikangalgouda@gmail.com>
2021-01-05 17:12:54 +0530

    [5th jan]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
+import './css/responsive.css';
 import Header from './components/Header';
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React , {useState} from 'react';
 import screenshot from '../assets/screenshot.jpeg';
 import playstore from '../assets/playstore.svg';
 import apple from '../assets/apple.svg';
 
 function Header() {
+    const [active, setActive] = useState(false);
     return (
         <header>
             <nav>
+                <div id="menuToggle" className="logo2" onClick={() => setActive(!active)}><i className="fa fa-bars"></i></div>
                 <div className="logo1"><a href="/">CowsOnCloud</a></div>
-                <div className="logo2"><i className="fa fa-bars"></i></div>
-                <div className="navigation">
+                <div className={active ? "navigation open" : "navigation"}>
                     <ul>
                         <li><a href="/">About</a></li>
                         <li><a href="/">Services</a></li>
@@ -19,23 +20,20 @@ function Header() {
                     </ul>
                 </div>
             </nav>
-            <div className="login-btn">
-                <div><a href="/">Login</a></div>
-            </div>
             <div className="download">
                 <div className="dow-container">
                     <div className="download-btn">
                         <p className="download-name">Click To Download</p>
                         <div className="btns">
                             <div className="google">
-                                <img src={playstore} alt="playstore" width="30px" />
+                                <img src={playstore} alt="playstore"/>
                                 <div className="btn-name">
                                     <p className="btn-name-get">GET IT ON</p>
                                     <a className="btn-name-google" href="#download">Google play</a>
                                 </div>
                             </div>
                             <div className="apple">
-                                <img src={apple} alt="apple" width="30px" />
+                                <img src={apple} alt="apple"/>
                                 <div className="btn-name">
                                     <p className="btn-name-on">Download on the</p>
                                     <a className="btn-name-apple" href="#download">Apple Store</a>
